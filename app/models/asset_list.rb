4de80e0d5230d5ac0c576558ab72d22b6b5f879a
r.nishio <dummy>
2013-05-29 00:25:38 +0900

    asset 取得バッチ新規作成

---
+++ app/models/asset_list.rb
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+class AssetLists
+  include DataMapper::Resource
+
+  storage_names[:default] = 'AssetList'
+  property :item_id,Integer, field: 'itemID', key:true, required: true
+  property :quantity,Integer, field: 'quantity'
+  property :type_id,Integer, field: 'typeID'
+  property :location_id,Integer, field: 'locationID'
+  property :flag,Integer, field:'flag'
+
+  class << self
+    def bulk_persist!(transaction)
+      transaction.each do |tran|
+        begin
+          puts tran.item_id
+          tran.save
+        rescue
+          puts "DB登録の際にエラーが発生しました"
+        end
+      end
+    end
+  end
+end
