607018b02b323a53a775738f459f8a5302b2403a
S.nishio <nishio@densan-labs.net>
2013-06-16 00:27:01 +0900

    model: translation modelを追加

---
+++ app/models/trn_translations.rb
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+class TrnTranslations
+  include DataMapper::Resource
+
+  storage_names[:default] = 'trnTranslations'
+
+  property :tc_id, Serial, field: 'tcID', required: true, key: true
+  property :key_id, Serial, field: 'keyID', required: true, key: true
+  property :language_id, String, field: 'languageID'
+  property :text, Text, field: 'text'
+
+end
