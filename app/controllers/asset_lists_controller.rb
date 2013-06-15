cdd85b92941f542da01129d7a6fca91c9814a5b2
S.nishio <nishio@densan-labs.net>
2013-06-16 01:21:10 +0900

    controller: refactor asset list

--- app/controllers/asset_lists_controller.rb
+++ app/controllers/asset_lists_controller.rb
@@ -1,28 +1,16 @@
+# coding: utf-8
+
 class AssetListsController < ApplicationController
-  # GET /asset_lists
-  # GET /asset_lists.json
+
   def index
     @asset_lists = AssetLists.all(:order => [ :type_id.asc])
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @asset_lists }
-    end
   end
 
-  # GET /asset_lists/1
-  # GET /asset_lists/1.json
   def show
     @asset_list = AssetLists.get(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @asset_list }
-    end
   end
 
   def search
-
     @asset_lists = if params[:s_type] != ""
                      AssetLists.all(:type_id => "#{InvTypes.first(:type_name.like => "%#{params[:s_type]}%")[:type_id]}")
                    elsif params[:s_flag] != ""
@@ -32,11 +20,9 @@ class AssetListsController < ApplicationController
                    end
     render :action => 'index'
   end
+
   private
 
-  # Use this method to whitelist the permissible parameters. Example:
-  # params.require(:person).permit(:name, :age)
-  # Also, you can specialize this method with per-user checking of permissible attributes.
   def asset_list_params
     params.require(:asset_list).permit(:flag, :item_id, :location_id, :quantity, :type_id)
   end
