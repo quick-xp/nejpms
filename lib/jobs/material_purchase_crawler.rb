0a66ee4b459e174732a70ec2185f63fc024e4e01
r.nishio <dummy>
2013-06-18 00:39:34 +0900

    #6: 原料仕入れマスタの不備 修正

--- lib/jobs/material_purchase_crawler.rb
+++ lib/jobs/material_purchase_crawler.rb
@@ -8,19 +8,21 @@ class Jobs::MaterialPurchaseCrawler
       puts "start MetarialPurchaseCrawler"
 
       wallet_transactions.map {|transaction|
-       if MaterialPurchase.first(:transaction_id => transaction[:transactionID]) == nil then
-         m = MaterialPurchase.new
-         m.transaction_id = transaction[:transactionID]
-         m.quantity = transaction[:quantity]
-         m.type_id = transaction[:typeID]
-         m.price = transaction[:price]
-         m.station_id = transaction[:stationID]
-         m.save
+        if transaction[:transactionType] == "buy" then
+          if MaterialPurchase.first(:transaction_id => transaction[:transactionID]) == nil then
+            m = MaterialPurchase.new
+            m.transaction_id = transaction[:transactionID]
+            m.quantity = transaction[:quantity]
+            m.type_id = transaction[:typeID]
+            m.price = transaction[:price]
+            m.station_id = transaction[:stationID]
+            m.save
 
-         puts "transaction_id " + m.transaction_id.to_s + " save"
-       else
-         puts "transaction_id" + transaction.transactionID.to_s + " is already saved"
-       end
+            puts "transaction_id " + m.transaction_id.to_s + " save"
+          else
+            puts "transaction_id" + transaction.transactionID.to_s + " is already saved"
+          end
+        end
       }
     rescue => e
       puts "Material Purchase Crawler error occures : " + e.message
