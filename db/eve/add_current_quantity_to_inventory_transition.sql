4fa53418e5d4d1def22411ec0d94640ccf8627f5
r.nishio <dummy>
2013-06-22 03:00:34 +0900

    在庫推移 DBテーブル名違いを修正

--- db/eve/add_current_quantity_to_inventory_transition.sql
+++ db/eve/add_current_quantity_to_inventory_transition.sql
@@ -1,23 +1,23 @@
 -- 現在の在庫数を保存できるようにする
-alter table inventoryTransition add column currentQuantity integer not null after `count`;
+alter table InventoryTransition add column currentQuantity integer not null after `count`;
 
 -- 6/15 より前のデータは不正なもののため削除
 delete from
-  inventoryTransition
+  InventoryTransition
 where
   addDate < '2013-06-15 00:00:00';
 
 -- 移行用。在庫数の推移から、その時点の在庫数を計算する
 update
-  inventoryTransition as i3
+  InventoryTransition as i3
 inner join (
   select
     i1.id,
     sum(i2.count) as summary
   from
-    inventoryTransition as i1
+    InventoryTransition as i1
   inner join
-    inventoryTransition as i2
+    InventoryTransition as i2
   on
     i1.typeID = i2.typeID
   where
